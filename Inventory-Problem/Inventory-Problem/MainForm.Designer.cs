90c48088cfe3422eb9ab900144d182f2f3c9f371
CookieTheory <118179323+CookieTheory@users.noreply.github.com>
2023-04-20 02:35:58 +0200

    Language localization test successful.

--- Inventory-Problem/Inventory-Problem/MainForm.Designer.cs
+++ Inventory-Problem/Inventory-Problem/MainForm.Designer.cs
@@ -28,19 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxLanguageSelect = new System.Windows.Forms.ComboBox();
+            this.languageLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(339, 199);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // comboBoxLanguageSelect
+            // 
+            this.comboBoxLanguageSelect.FormattingEnabled = true;
+            this.comboBoxLanguageSelect.Location = new System.Drawing.Point(667, 12);
+            this.comboBoxLanguageSelect.Name = "comboBoxLanguageSelect";
+            this.comboBoxLanguageSelect.Size = new System.Drawing.Size(121, 23);
+            this.comboBoxLanguageSelect.TabIndex = 1;
+            this.comboBoxLanguageSelect.SelectedIndexChanged += new System.EventHandler(this.comboBoxLanguageSelect_SelectedIndexChanged);
+            // 
+            // languageLabel
+            // 
+            this.languageLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.languageLabel.AutoSize = true;
+            this.languageLabel.Location = new System.Drawing.Point(599, 15);
+            this.languageLabel.Name = "languageLabel";
+            this.languageLabel.Size = new System.Drawing.Size(62, 15);
+            this.languageLabel.TabIndex = 2;
+            this.languageLabel.Text = "Language:";
+            this.languageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.languageLabel);
+            this.Controls.Add(this.comboBoxLanguageSelect);
+            this.Controls.Add(this.label1);
             this.Name = "MainForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label label1;
+        private ComboBox comboBoxLanguageSelect;
+        private Label languageLabel;
     }
 }
--- Inventory-Problem/Inventory-Problem/MainForm.cs
+++ Inventory-Problem/Inventory-Problem/MainForm.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace Inventory_Problem
 {
     public partial class MainForm : Form
@@ -5,6 +7,35 @@ namespace Inventory_Problem
         public MainForm()
         {
             InitializeComponent();
+            Start();
+            AddText();
+            RefreshLanguageStrings();
+        }
+
+        private void Start()
+        {
+            this.comboBoxLanguageSelect.DataSource = new string[] { "Hrvatski", "English" };
+        }
+
+        private void RefreshLanguageStrings()
+        {
+            this.label1.Text = Properties.strings.Hello;
+            this.languageLabel.Text = Properties.strings.Language;
+        }
+
+        private void AddText()
+        {
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("hr");
+            this.label1.Text = Properties.strings.Hello;
+        }
+
+        private void comboBoxLanguageSelect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string? language = comboBoxLanguageSelect.SelectedItem.ToString();
+            if (language == "Hrvatski") language = "hr";
+            else language = "en";
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo(language);
+            RefreshLanguageStrings();
         }
     }
 }
